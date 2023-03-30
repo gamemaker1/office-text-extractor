494d0d85b4675e9b53873f65df69d6d7d509fa90
Vedant K <gamemaker0042@gmail.com>
2023-03-30 16:25:57 +0530

    stash: begin rewrite

--- source/index.d.ts
+++ source/index.d.ts
@@ -1,5 +1,5 @@
-// Source/index.d.ts
+// source/index.d.ts
 // Type definitions for the library.
 
 // See `source/index.js` for the JSDoc for this function.
-export declare function extractText(filePath: string): Promise<string>
+export declare function extractText(): Promise<string>
--- source/index.js
+++ source/index.js
@@ -1,50 +1,32 @@
-// Source/index.js
+// source/index.js
 // This file contains all the public functions that the library exposes.
 
-import { readFile } from 'node:fs/promises'
+/**
+ * The text extractor class.
+ */
+export class TextExtractor {
+	methods = []
 
-import { fileTypeFromBuffer as getFileType } from 'file-type'
-import { readChunk } from 'read-chunk'
+	addMethod = (method) => this.methods.push(method)
+	extractText = ({ input, type }) => {
+		const extractor = this.methods.find((method) => method.type === type)
+		if (!extractor?.apply) {
+			const message = `text-extractor: could not find a method to handle ${type} input`
+			throw new Error(message)
+		}
 
-import { parse as parseTextFromWordFile } from './parsers/word.js'
-import { parse as parseTextFromPptFile } from './parsers/ppt.js'
-import { parse as parseTextFromExcelFile } from './parsers/excel.js'
-import { parse as parseTextFromPdfFile } from './parsers/pdf.js'
+		return extractor.apply(input)
+	}
+}
 
 /**
- * Extract text from a file if possible.
- *
- * @param {string} filePath - The path to the file to extract text from.
- *
- * @returns {Promise<string>} - The text extracted from the file.
- * @throws {UnsupportedMimeTypeError} - If the file type is unsupported.
+ * Create and returns a text extractor instance with the default extraction
+ * methods.
  */
-export const extractText = async (filePath) => {
-	// Use `readChunk` to get only a small part of the file. Usually, the 4200
-	// bytes are enough to determine the mime type.
-	const chunk = await readChunk(filePath, { length: 4200, startPosition: 0 })
-	// Call `getFileType` to determine the mime type from the buffer we read.
-	const mimeDetails = await getFileType(chunk)
-	// If the mime type library was not able to parse the file, it is most
-	// probably a file with plain text content. So return the file's content
-	// directly.
-	if (!mimeDetails) {
-		return readFile(filePath, 'utf8')
-	}
+export const getTextExtractor = async () => {
+	const textExtractor = new TextExtractor()
+	const methods = [await import('./parsers/pdf.js')]
+	methods.map((method) => textExtractor.addMethod(method))
 
-	// Check the mime type and call the appropriate function.
-	switch (mimeDetails.mime) {
-		case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-			return parseTextFromWordFile(filePath)
-		case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
-			return parseTextFromPptFile(filePath)
-		case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-			return parseTextFromExcelFile(filePath)
-		case 'application/pdf':
-			return parseTextFromPdfFile(filePath)
-		default:
-			throw new Error(
-				`Unsupported mime type ${mimeDetails.mime} (.${mimeDetails.ext})`,
-			)
-	}
+	return textExtractor
 }
